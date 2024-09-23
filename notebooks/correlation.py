f42e0046bc3bd860110527374cdd1e0fad6a6989
lassekh <31202809+lassekh@users.noreply.github.com>
2024-09-23 15:01:21 +0200

    changes fe

--- notebooks/correlation.py
+++ notebooks/correlation.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 
-def interest_sp500(days, df):
+def fe_interest_changes(days, df):
     # Calculate interest rate changes an save in new column
-    df['InterestRateChange'] = df['Effective Federal Funds Rate'].diff()
+    df['InterestRateChange'] = df['InterestRate'].diff()
     # Flag days where there is a change in interest rate with 1
     df['InterestRateChangeFlag'] = np.where(df['InterestRateChange'] != 0, 1, 0)
     # Calculate the change of the "Close" value X days from the interest rate change
     # and place it the same number of days back on the potential interest change day
     df[f'{days}_DAYS_Change'] = df['Close'].pct_change(periods=days).shift(-days)
     # Drop NaN rows
-    df = df.dropna(subset=['InterestRateChange', 'SP500_10DAY_Change'])
+    # df = df.dropna(subset=['InterestRateChange', 'SP500_10DAY_Change'])
     # Find all points with a change in interest rate
-    df_rate_change = df[df['InterestRateChangeFlag'] == 1].copy()
-    return df_rate_change
+    # df_rate_change = df[df['InterestRateChangeFlag'] == 1].copy()
+    return df
